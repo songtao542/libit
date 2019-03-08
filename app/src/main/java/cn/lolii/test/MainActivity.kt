951236d226564bb41e79bee7be431d9c8bace991
songtao.wang <songtao.wang@ff.com>
2019-03-08 19:41:44 +0800

    create statebutton

---
+++ app/src/main/java/cn/lolii/test/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.lolii.test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.lolii.test.test.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ statebutton/src/main/java/cn/lolii/statebutton/CircleStateButton.java
@@ -0,0 +1,94 @@
+package cn.lolii.statebutton;
+
+import android.content.Context;
+import android.graphics.BlurMaskFilter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import cn.lolii.statebutton.MaterialProgressDrawable.Shape;
+
+@SuppressWarnings("unused")
+public class CircleStateButton extends StateButton {
+
+    public CircleStateButton(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CircleStateButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CircleStateButton(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+
+    public CircleStateButton(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private MaterialProgressDrawable mAnimatorDrawable = null;
+
+    private void init(Context context) {
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        Drawable backgroundDrawable = getResources().getDrawable(R.drawable.circle_state_button_background, context.getTheme());
+        mAnimatorDrawable = new MaterialProgressDrawable(context, this, 64.0, 64.0, 0.0, 1.2, Shape.CIRCLE);
+        int color = Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1 ?
+                getResources().getColor(R.color.progress_color, context.getTheme()) : getResources().getColor(R.color.progress_color);
+        mAnimatorDrawable.setColorSchemeColors(color);
+        mAnimatorDrawable.setBlurMaskFilter(20f, BlurMaskFilter.Blur.SOLID);
+        //glowDrawable.setShadowLayer(20f, 0xffffff)
+        if (backgroundDrawable instanceof LayerDrawable) {
+            ((LayerDrawable) backgroundDrawable).setDrawableByLayerId(R.id.animator_layer, mAnimatorDrawable);
+        }
+        setBackground(backgroundDrawable);
+    }
+
+
+    public void handleState(State oldState, State newState) {
+        if (mAnimatorDrawable != null) {
+            switch (newState) {
+                case OFF: {
+                    mAnimatorDrawable.setVisible(false);
+                }
+                case ON: {
+                    mAnimatorDrawable.setVisible(true);
+                    mAnimatorDrawable.stop();
+                }
+                case LOADING: {
+                    Log.d("TTTT", "handleState loading");
+                    mAnimatorDrawable.setVisible(true);
+                    mAnimatorDrawable.start();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (state == State.LOADING && mAnimatorDrawable != null) {
+            mAnimatorDrawable.start();
+        }
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (state == State.LOADING && mAnimatorDrawable != null) {
+            mAnimatorDrawable.stop();
+        }
+    }
+
+}

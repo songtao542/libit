d0090b9c3cce07e318a68837ffb4bd46d5c73336
wangsongtao <wangsongtao@domobile.com>
2019-05-19 00:35:44 +0800

    add shimmer layout

---
+++ app/src/main/java/cn/lolii/test/ShimmerTestActivity.kt
@@ -0,0 +1,18 @@
+package cn.lolii.test
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cn.lolii.test.test.R
+import com.lolii.shimmer.ShimmerLayout
+
+
+class ShimmerTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shimmer_test)
+
+        val shimmerLayout = findViewById<ShimmerLayout>(R.id.shimmerLayout)
+    }
+}

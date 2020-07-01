859569bae54f82e162f523029d3f5046b0b41840
wangsongtao <wst@cn.gree.com>
2020-07-01 21:28:25 +0800

    add wallpaper cropper

---
+++ wallpaper_cropper/src/main/java/com/android/gallery3d/exif/IfdId.java
@@ -0,0 +1,15 @@
+package com.android.gallery3d.exif;
+
+/**
+ * The constants of the IFD ID defined in EXIF spec.
+ */
+public interface IfdId {
+    int TYPE_IFD_0 = 0;
+    int TYPE_IFD_1 = 1;
+    int TYPE_IFD_EXIF = 2;
+    int TYPE_IFD_INTEROPERABILITY = 3;
+    int TYPE_IFD_GPS = 4;
+    /* This is used in ExifData to allocate enough IfdData */
+    int TYPE_IFD_COUNT = 5;
+
+}

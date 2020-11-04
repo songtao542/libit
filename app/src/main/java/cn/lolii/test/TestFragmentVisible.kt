a6c8457d9f2f7855c569c24559e5a43b5fc060e3
wangsongtao <wst@cn.gree.com>
2020-11-04 09:32:05 +0800

    add test

---
+++ app/src/main/java/cn/lolii/test/TestFragmentVisible.kt
@@ -0,0 +1,72 @@
+package cn.lolii.test
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import kotlinx.android.synthetic.main.activity_test_fragment_visible.*
+import kotlinx.android.synthetic.main.activity_test_fragment_visible.tabLayout
+
+class TestFragmentVisible : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_fragment_visible)
+        viewPager.adapter = FAdapter(supportFragmentManager)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    class FAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return "$position"
+        }
+
+        override fun getCount(): Int {
+            return 10
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return TestFragment(position)
+        }
+
+    }
+
+
+    class TestFragment(val position: Int) : Fragment(), View.OnAttachStateChangeListener {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val view = inflater.inflate(R.layout.activity_test_fragment_visible, container, false)
+            view.addOnAttachStateChangeListener(this)
+            return view
+        }
+
+        override fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+            Log.d("TTTT", "$position onActivityCreated isVisible: $isVisible")
+        }
+
+        override fun onAttach(context: Context) {
+            super.onAttach(context)
+            Log.d("TTTT", "$position onAttach isVisible: $isVisible")
+        }
+
+        override fun onDetach() {
+            super.onDetach()
+            Log.d("TTTT", "$position onDetach isVisible: $isVisible")
+        }
+
+        override fun onViewAttachedToWindow(v: View?) {
+            Log.d("TTTT", "$position attached to window")
+        }
+
+        override fun onViewDetachedFromWindow(v: View?) {
+            Log.d("TTTT", "$position detached to window")
+        }
+    }
+}

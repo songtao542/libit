cb5371711c41e113f4f477b65d6a98c761ac6927
wangsongtao <wst@cn.gree.com>
2020-11-06 17:09:40 +0800

    add recyclerview_decoration test

---
+++ app/src/main/java/com/liabit/test/RecyclerViewDecorationTestActivity.kt
@@ -0,0 +1,11 @@
+package com.liabit.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerViewDecorationTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view_decoration_test)
+    }
+}

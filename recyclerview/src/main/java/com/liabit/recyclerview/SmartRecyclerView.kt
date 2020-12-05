12fdb98774bbd71ce7ba3d7fdc6d868f13aa0fcc
songtao.wang <songtao.wang@ff.com>
2020-12-05 13:09:16 +0800

    fix bug

--- recyclerview/src/main/java/com/liabit/recyclerview/SmartRecyclerView.kt
+++ recyclerview/src/main/java/com/liabit/recyclerview/SmartRecyclerView.kt
@@ -1,27 +1,27 @@
-package com.liabit.recyclerview
-
-import android.content.Context
-import android.util.AttributeSet
-import androidx.recyclerview.widget.RecyclerView
-
-/**
- * Author:         songtao
- * CreateDate:     2020/11/17 12:55
- */
-class SmartRecyclerView : RecyclerView {
-    constructor(context: Context) : super(context) {
-        init(context, null, 0)
-    }
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init(context, attrs, 0)
-    }
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(context, attrs, defStyleAttr)
-    }
-
-    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
-
-    }
-}
+//package com.liabit.recyclerview
+//
+//import android.content.Context
+//import android.util.AttributeSet
+//import androidx.recyclerview.widget.RecyclerView
+//
+///**
+// * Author:         songtao
+// * CreateDate:     2020/11/17 12:55
+// */
+//class SmartRecyclerView : RecyclerView {
+//    constructor(context: Context) : super(context) {
+//        init(context, null, 0)
+//    }
+//
+//    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+//        init(context, attrs, 0)
+//    }
+//
+//    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+//        init(context, attrs, defStyleAttr)
+//    }
+//
+//    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+//
+//    }
+//}

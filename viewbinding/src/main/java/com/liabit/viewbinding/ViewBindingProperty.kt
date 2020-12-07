0912fef0317e2617f5db017934dd1379c4fd555d
wangsongtao <wst@cn.gree.com>
2020-12-07 13:49:46 +0800

    fix bug

--- viewbinding/src/main/java/com/liabit/viewbinding/ViewBindingProperty.kt
+++ viewbinding/src/main/java/com/liabit/viewbinding/ViewBindingProperty.kt
@@ -26,9 +26,9 @@ inline fun <reified VB : ViewBinding> inflate(): ReadOnlyProperty<Any, VB> {
     return ViewBindingProperty(null, VB::class.java)
 }
 
-/*inline fun <reified VB : ViewBinding> bind(view: View): ReadOnlyProperty<Any, VB> {
+inline fun <reified VB : ViewBinding> bind(view: View): ReadOnlyProperty<Any, VB> {
     return ViewBindingProperty({ view }, VB::class.java)
-}*/
+}
 
 inline fun <reified VB : ViewBinding> bind(noinline viewProvider: () -> View): ReadOnlyProperty<Any, VB> {
     return ViewBindingProperty(viewProvider, VB::class.java)

ed8b56208c28406de184763f269f39e9178c7cbb
wangsongtao <wst@cn.gree.com>
2021-01-20 22:14:27 +0800

    fix bug

--- numberpicker/src/main/java/com/liabit/numberpicker/PickerFragment.kt
+++ numberpicker/src/main/java/com/liabit/numberpicker/PickerFragment.kt
@@ -5,171 +5,129 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import java.util.*
 
 @Suppress("unused")
 class PickerFragment : BottomSheetDialogFragment() {
 
-    companion object {
-        const val EXTRA_TITLE = "title"
-        const val EXTRA_MIN = "min_value"
-        const val EXTRA_MAX = "max_value"
-        const val EXTRA_COLUMN_1_VALUES = "column1"
-        const val EXTRA_COLUMN_2_VALUES = "column2"
-        const val EXTRA_SHOW_PROGRESS = "progress"
-
-        @JvmStatic
-        fun newInstance(title: String) = PickerFragment().apply {
-            arguments = Bundle().apply {
-                this.putString(EXTRA_TITLE, title)
-                this.putBoolean(EXTRA_SHOW_PROGRESS, true)
-            }
-        }
-
-        @JvmStatic
-        fun newInstance(title: String, minValue: Int, maxValue: Int) = PickerFragment().apply {
-            arguments = Bundle().apply {
-                this.putString(EXTRA_TITLE, title)
-                this.putInt(EXTRA_MIN, minValue)
-                this.putInt(EXTRA_MAX, maxValue)
-            }
-        }
-
-        @JvmStatic
-        fun newInstance(title: String, values: Array<String>) = PickerFragment().apply {
-            arguments = Bundle().apply {
-                this.putString(EXTRA_TITLE, title)
-                this.putStringArray(EXTRA_COLUMN_1_VALUES, values)
-            }
-        }
-
-        @JvmStatic
-        fun newInstance(title: String, column1: Array<String>, column2: Array<String>) = PickerFragment().apply {
-            arguments = Bundle().apply {
-                this.putString(EXTRA_TITLE, title)
-                this.putStringArray(EXTRA_COLUMN_1_VALUES, column1)
-                this.putStringArray(EXTRA_COLUMN_2_VALUES, column2)
-            }
-        }
-    }
-
-    private var column1View: NumberPicker? = null
-    private var column2View: NumberPicker? = null
-    private var title: TextView? = null
-    private var confirm: TextView? = null
-    private var progress: View? = null
-    private var pickers: View? = null
+    private var mColumn1View: NumberPicker? = null
+    private var mColumn2View: NumberPicker? = null
+    private var mTitleTextView: TextView? = null
+    private var mConfirmButton: TextView? = null
+    private var mProgressView: View? = null
+    private var mPickersWrapLayout: View? = null
+    private var mOnValueListener: ((value1: CharSequence, value2: CharSequence) -> Unit)? = null
+    private var mOnResultListener: ((value1: Int, value2: Int) -> Unit)? = null
+    private var mTitle: CharSequence? = null
+    private var mTitleResId: Int? = null
+    private var mShowProgress: Boolean = false
+    private var mValue1: Int = 0
+    private var mValue2: Int = 0
+    private var mMinValue: Int? = null
+    private var mMaxValue: Int? = null
+    private var mColumn1Values: Array<out CharSequence>? = null
+    private var mColumn2Values: Array<out CharSequence>? = null
+    private var mColumns: LinkedHashMap<out CharSequence, out List<CharSequence>>? = null
+    private var mColumn2SubOfColumn1Type = -1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.p_fragment_picker, container, false)
-        title = view.findViewById(R.id.title)
-        confirm = view.findViewById(R.id.confirm)
-        progress = view.findViewById(R.id.progress)
-        pickers = view.findViewById(R.id.pickers)
-        column1View = view.findViewById(R.id.column1View)
-        column2View = view.findViewById(R.id.column2View)
+        mTitleTextView = view.findViewById(R.id.title)
+        mConfirmButton = view.findViewById(R.id.confirm)
+        mProgressView = view.findViewById(R.id.progress)
+        mPickersWrapLayout = view.findViewById(R.id.pickers)
+        mColumn1View = view.findViewById(R.id.column1View)
+        mColumn2View = view.findViewById(R.id.column2View)
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        column1View?.let {
+        mColumn1View?.let {
             it.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
             it.wrapSelectorWheel = false
             it.setDividerHeight(0.4f)
             it.setDividerColor(requireContext().colorOf(R.color.p_picker_title))
         }
 
-        column2View?.let {
+        mColumn2View?.let {
             it.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
             it.wrapSelectorWheel = false
             it.setDividerHeight(0.4f)
             it.setDividerColor(requireContext().colorOf(R.color.p_picker_title))
         }
 
-        arguments?.let {
-            val title = it.getString(EXTRA_TITLE)
-            this.title?.text = title ?: getString(R.string.np_please_select)
-            val showProgress = it.getBoolean(EXTRA_SHOW_PROGRESS, false)
-            showProgress(showProgress)
+        mTitleTextView?.text = mTitle ?: getString(mTitleResId ?: R.string.np_please_select)
+        showProgress(mShowProgress)
 
-            val minValue = it.getInt(EXTRA_MIN, -1)
-            val maxValue = it.getInt(EXTRA_MAX, -1)
-            if (minValue != -1 && maxValue != -1) {
-                setColumnInternal(minValue, maxValue)
-            }
-
-            val column1Values = it.getStringArray(EXTRA_COLUMN_1_VALUES)
-            val column2Values = it.getStringArray(EXTRA_COLUMN_2_VALUES)
-            if (column1Values != null) {
-                this.column1Values = column1Values
-            }
-            if (column2Values != null) {
-                this.column2Values = column2Values
-            }
-            if (column1Values != null || column2Values != null) {
-                setColumnInternal(column1Values, column2Values)
-            }
+        val minValue = mMinValue
+        val maxValue = mMaxValue
+        if (minValue != null && maxValue != null) {
+            setColumnInternal(minValue, maxValue)
         }
 
-        confirm?.setOnClickListener {
+        if (mColumn1Values != null || mColumn2Values != null) {
+            setColumnInternal(mColumn1Values, mColumn2Values)
+        }
+
+        mConfirmButton?.setOnClickListener {
             dismiss()
-            val index1 = column1View?.value ?: return@setOnClickListener
-            val index2 = column2View?.value ?: return@setOnClickListener
-            onResultListener?.invoke(index1, index2)
-            val value1 = column1View?.displayedValues?.get(index1) ?: return@setOnClickListener
-            val value2 = column2View?.displayedValues?.get(index2) ?: return@setOnClickListener
-            onValueListener?.invoke(value1, value2)
+            val index1 = mColumn1View?.value ?: return@setOnClickListener
+            val index2 = mColumn2View?.value ?: return@setOnClickListener
+            mOnResultListener?.invoke(index1, index2)
+            val value1 = mColumn1View?.displayedValues?.get(index1) ?: return@setOnClickListener
+            val value2 = mColumn2View?.displayedValues?.get(index2) ?: return@setOnClickListener
+            mOnValueListener?.invoke(value1, value2)
         }
     }
-
-    private var onResultListener: ((value1: Int, value2: Int) -> Unit)? = null
 
     fun setOnResultListener(onResultListener: ((value: Int, value2: Int) -> Unit)) {
-        this.onResultListener = onResultListener
+        this.mOnResultListener = onResultListener
     }
-
-    private var onValueListener: ((value1: CharSequence, value2: CharSequence) -> Unit)? = null
 
     fun setOnValueListener(onValueListener: ((value: CharSequence, value2: CharSequence) -> Unit)) {
-        this.onValueListener = onValueListener
+        this.mOnValueListener = onValueListener
     }
 
-    var value1: Int = 0
-    var value2: Int = 0
+    fun setTitle(title: CharSequence) {
+        mTitle = title
+        mTitleTextView?.text = title
+    }
 
-    private var minValue: Int = -1
-    private var maxValue: Int = -1
+    fun setTitle(titleResId: Int) {
+        mTitleResId = titleResId
+        mTitleTextView?.setText(titleResId)
+    }
 
-    private var column1Values: Array<out CharSequence>? = null
-    private var column2Values: Array<out CharSequence>? = null
-
-    private var columns: LinkedHashMap<out CharSequence, out List<CharSequence>>? = null
+    fun setShowProgress(showProgress: Boolean) {
+        mShowProgress = showProgress
+    }
 
     private fun showProgress(show: Boolean) {
         if (show) {
-            progress?.visibility = View.VISIBLE
-            pickers?.visibility = View.GONE
+            mProgressView?.visibility = View.VISIBLE
+            mPickersWrapLayout?.visibility = View.GONE
         } else {
-            progress?.visibility = View.GONE
-            pickers?.visibility = View.VISIBLE
+            mProgressView?.visibility = View.GONE
+            mPickersWrapLayout?.visibility = View.VISIBLE
         }
     }
 
     fun setColumn(minValue: Int, maxValue: Int) {
-        this.minValue = minValue
-        this.maxValue = maxValue
+        mMinValue = minValue
+        mMaxValue = maxValue
         setColumnInternal(minValue, maxValue)
     }
 
     private fun setColumnInternal(minValue: Int, maxValue: Int) {
-        val cv = column1View ?: return
+        val cv = mColumn1View ?: return
         if (minValue >= 0 && maxValue >= 0) {
             cv.minValue = minValue
             cv.maxValue = maxValue
-            if (value1 in minValue..maxValue) {
-                cv.value = value1
+            if (mValue1 in minValue..maxValue) {
+                cv.value = mValue1
             }
             showProgress(false)
         }
@@ -177,12 +135,14 @@ class PickerFragment : BottomSheetDialogFragment() {
 
     override fun onResume() {
         super.onResume()
-        if (minValue != -1 && maxValue != -1) {
-            setColumnInternal(minValue, maxValue)
-        } else if (column1Values != null || column2Values != null) {
-            setColumnInternal(column1Values, column2Values)
-        } else if (columns != null) {
-            setColumnInternal(columns)
+        val minValue = mMinValue
+        val maxValue = mMaxValue
+        if (minValue != null && maxValue != null) {
+            setColumnInternal(minValue, minValue)
+        } else if (mColumn1Values != null || mColumn2Values != null) {
+            setColumnInternal(mColumn1Values, mColumn2Values)
+        } else if (mColumns != null) {
+            setColumnInternal(mColumns)
         }
         if (mColumn2SubOfColumn1Type >= 0) {
             setColumn2SubOfColumn1Internal()
@@ -190,21 +150,21 @@ class PickerFragment : BottomSheetDialogFragment() {
     }
 
     fun setColumn(column1Values: Array<CharSequence>?, column2Values: Array<CharSequence>? = null) {
-        this.column1Values = column1Values
-        this.column2Values = column2Values
+        this.mColumn1Values = column1Values
+        this.mColumn2Values = column2Values
         setColumnInternal(column1Values, column2Values)
     }
 
     private fun setColumnInternal(column1Values: Array<out CharSequence>?, column2Values: Array<out CharSequence>? = null) {
         if (column1Values != null) {
             //先置空，以免数组越界
-            column1View?.let {
+            mColumn1View?.let {
                 it.displayedValues = null
                 it.minValue = 0
                 it.maxValue = column1Values.size - 1
                 it.displayedValues = column1Values
-                if (value1 >= 0 && value1 < column1Values.size) {
-                    it.value = value1
+                if (mValue1 >= 0 && mValue1 < column1Values.size) {
+                    it.value = mValue1
                 } else {
                     it.value = 0
                 }
@@ -213,15 +173,15 @@ class PickerFragment : BottomSheetDialogFragment() {
         }
 
         if (column2Values != null) {
-            column2View?.let {
+            mColumn2View?.let {
                 it.visibility = View.VISIBLE
                 //先置空，以免数组越界
                 it.displayedValues = null
                 it.minValue = 0
                 it.maxValue = column2Values.size - 1
                 it.displayedValues = column2Values
-                if (value2 >= 0 && value2 < column2Values.size) {
-                    it.value = value2
+                if (mValue2 >= 0 && mValue2 < column2Values.size) {
+                    it.value = mValue2
                 } else {
                     it.value = 0
                 }
@@ -229,8 +189,6 @@ class PickerFragment : BottomSheetDialogFragment() {
             }
         }
     }
-
-    private var mColumn2SubOfColumn1Type = -1
 
     /**
      * @param column2SubOfColumn1Type 0:取前半部分; 大于 0: 取后半部分
@@ -241,15 +199,15 @@ class PickerFragment : BottomSheetDialogFragment() {
     }
 
     private fun setColumn2SubOfColumn1Internal() {
-        val column1Values = column1Values ?: return
+        val column1Values = mColumn1Values ?: return
         val array: Array<out CharSequence> = if (mColumn2SubOfColumn1Type == 0) {
             column1Values.copyOfRange(0, 1)
         } else {
             column1Values.copyOfRange(1, column1Values.size)
         }
         setColumnInternal(null, array)
-        column1View?.setOnValueChangedListener { _, _, newVal ->
-            val c1vs = this.column1Values ?: return@setOnValueChangedListener
+        mColumn1View?.setOnValueChangedListener { _, _, newVal ->
+            val c1vs = this.mColumn1Values ?: return@setOnValueChangedListener
             val vs: Array<out CharSequence> = if (mColumn2SubOfColumn1Type == 0) {
                 c1vs.copyOfRange(0, if (newVal == 0) 1 else newVal)
             } else {
@@ -263,17 +221,17 @@ class PickerFragment : BottomSheetDialogFragment() {
      * 使用 LinkedHashMap 是为了保持插入顺序
      */
     fun setColumn(columns: LinkedHashMap<out CharSequence, out List<CharSequence>>) {
-        this.columns = columns
+        this.mColumns = columns
         setColumnInternal(columns)
     }
 
     private fun setColumnInternal(columns: LinkedHashMap<out CharSequence, out List<CharSequence>>?) {
-        val column1View = column1View ?: return
+        val column1View = mColumn1View ?: return
         if (!columns.isNullOrEmpty()) {
-            column1Values = columns.keys.toTypedArray().also {
+            mColumn1Values = columns.keys.toTypedArray().also {
                 // 设置第一列数据
                 setColumnInternal(it, null)
-                val index1 = if (value1 >= 0 && value1 < it.size) value1 else 0
+                val index1 = if (mValue1 >= 0 && mValue1 < it.size) mValue1 else 0
                 columns[it[index1]]?.toTypedArray()?.let { array ->
                     // 设置第二列数据
                     setColumnInternal(null, array)
@@ -286,6 +244,56 @@ class PickerFragment : BottomSheetDialogFragment() {
                 }
             }
         }
+    }
+
+    class Builder {
+        private val mPicker = PickerFragment()
+
+        fun setTitle(title: CharSequence): Builder {
+            mPicker.setTitle(title)
+            return this
+        }
+
+        fun setTitle(titleResId: Int): Builder {
+            mPicker.setTitle(titleResId)
+            return this
+        }
+
+        fun setShowProgress(showProgress: Boolean): Builder {
+            mPicker.setShowProgress(showProgress)
+            return this
+        }
+
+        fun setOnResultListener(onResultListener: ((value: Int, value2: Int) -> Unit)): Builder {
+            mPicker.setOnResultListener(onResultListener)
+            return this
+        }
+
+        fun setOnValueListener(onValueListener: ((value: CharSequence, value2: CharSequence) -> Unit)): Builder {
+            mPicker.setOnValueListener(onValueListener)
+            return this
+        }
+
+        fun setColumn(minValue: Int, maxValue: Int): Builder {
+            mPicker.setColumn(minValue, maxValue)
+            return this
+        }
+
+        fun setColumn2SubOfColumn1(column2SubOfColumn1Type: Int): Builder {
+            mPicker.setColumn2SubOfColumn1(column2SubOfColumn1Type)
+            return this
+        }
+
+        fun setColumn(columns: LinkedHashMap<out CharSequence, out List<CharSequence>>): Builder {
+            mPicker.setColumn(columns)
+            return this
+        }
+
+        fun show(fragmentManager: FragmentManager): PickerFragment {
+            mPicker.show(fragmentManager, "np")
+            return mPicker
+        }
+
     }
 
 }

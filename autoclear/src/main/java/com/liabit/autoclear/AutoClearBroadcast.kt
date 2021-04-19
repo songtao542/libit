36bb468a59153d96ec388f3469a92932ab8a97a3
wangsongtao <wst@cn.gree.com>
2021-04-19 14:18:29 +0800

    fix bug

--- autoclear/src/main/java/com/liabit/autoclear/AutoClearBroadcast.kt
+++ autoclear/src/main/java/com/liabit/autoclear/AutoClearBroadcast.kt
@@ -6,16 +6,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Bundle
 import android.util.ArrayMap
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 /**
  *
  */
 @Suppress("unused")
-class AutoClearBroadcastRegistrar : Clearable {
+class BroadcastRegistry : Clearable {
 
     private var receivers = ArrayMap<Context, BroadcastReceiver>()
 
@@ -27,13 +28,7 @@ class AutoClearBroadcastRegistrar : Clearable {
     }
 
     fun registerReceiver(context: Context, action: String, receiver: ((context: Context?, intent: Intent?) -> Unit)) {
-        val realReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                receiver.invoke(context, intent)
-            }
-        }
-        receivers[context] = realReceiver
-        LocalBroadcastManager.getInstance(context).registerReceiver(realReceiver, IntentFilter(buildAction(context, action)))
+        registerReceiver(context, listOf(action), receiver)
     }
 
     fun registerReceiver(context: Context, actions: List<String>, receiver: ((context: Context?, intent: Intent?) -> Unit)) {
@@ -47,18 +42,49 @@ class AutoClearBroadcastRegistrar : Clearable {
         for (action in actions) {
             intentFilter.addAction(buildAction(context, action))
         }
-        LocalBroadcastManager.getInstance(context).registerReceiver(realReceiver, IntentFilter())
+        LocalBroadcastManager.getInstance(context).registerReceiver(realReceiver, intentFilter)
     }
+}
+
+private fun buildAction(context: Context, action: String): String {
+    return "${context.packageName}_$action"
+}
+
+private var receivers = ArrayMap<Context, BroadcastRegistry>()
+
+fun Context.registerReceiver(receiver: ((context: Context?, intent: Intent?) -> Unit), vararg action: String) {
+    val registry = receivers[this] ?: BroadcastRegistry().also { receivers[this] = it }
+    if (this is LifecycleOwner) {
+        val lifecycleObserver = object : DefaultLifecycleObserver {
+            override fun onDestroy(owner: LifecycleOwner) {
+                owner.lifecycle.removeObserver(this)
+                for (value in receivers.values) {
+                    value.clear()
+                }
+            }
+        }
+        this.lifecycle.addObserver(lifecycleObserver)
+    }
+    registry.registerReceiver(this, listOf(*action), receiver)
+}
+
+fun Context.clearBroadcastReceiver() {
+    receivers[this]?.clear()
+}
+
+fun Context.registerReceiver(receiver: BroadcastReceiver, action: String) {
+    LocalBroadcastManager.getInstance(this)
+            .registerReceiver(receiver, IntentFilter(buildAction(this, action)))
+}
+
+fun Context.unregisterLocalBroadcastReceiver(receiver: BroadcastReceiver) {
+    LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
 }
 
 class BroadcastBuilder(private val context: Context) : Intent() {
     fun send() {
         LocalBroadcastManager.getInstance(context).sendBroadcast(this)
     }
-}
-
-private fun buildAction(context: Context, action: String): String {
-    return "${context.packageName}_$action"
 }
 
 fun Context.broadcast(action: String): BroadcastBuilder {
@@ -69,13 +95,4 @@ fun Context.broadcast(action: String): BroadcastBuilder {
 
 fun Fragment.broadcast(action: String): BroadcastBuilder? {
     return context?.broadcast(action)
-}
-
-fun Context.registerReceiver(receiver: BroadcastReceiver, action: String) {
-    LocalBroadcastManager.getInstance(this)
-            .registerReceiver(receiver, IntentFilter(buildAction(this, action)))
-}
-
-fun Context.unregisterLocalBroadcastReceiver(receiver: BroadcastReceiver) {
-    LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
 }

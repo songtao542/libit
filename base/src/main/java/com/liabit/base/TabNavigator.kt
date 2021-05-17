8375573ebd727732c3dfdb8601f3996e77238ee9
wangsongtao <wst@cn.gree.com>
2021-05-17 14:59:25 +0800

    add TabNavigator.kt

---
+++ base/src/main/java/com/liabit/base/TabNavigator.kt
@@ -0,0 +1,126 @@
+package com.liabit.base
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavDestination
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment
+
+/**
+ * 使用 detach 和 attach 来处理 tab 对应的Fragment
+ * <navigation /> 中使用 tab_fragment 替换原来的 fragment
+ *
+ * ```
+ * 使用示例:
+ * <navigation xmlns:android="http://schemas.android.com/apk/res/android"
+ *     xmlns:app="http://schemas.android.com/apk/res-auto"
+ *     xmlns:tools="http://schemas.android.com/tools"
+ *     app:startDestination="@+id/navigation_tab2">
+ *
+ *     <tab_fragment
+ *         android:id="@+id/navigation_tab1"
+ *         android:name="com.you.package.YourFragment1"
+ *         android:label="@string/navigation_tab1"
+ *         tools:layout="@layout/fragment_tab1" />
+ *
+ *     <tab_fragment
+ *         android:id="@+id/navigation_tab2"
+ *         android:name="com.you.package.YourFragment2"
+ *         android:label="@string/navigation_tab2"
+ *         tools:layout="@layout/fragment_tab2" />
+ *
+ *     <tab_fragment
+ *         android:id="@+id/navigation_tab3"
+ *         android:name="com.you.package.YourFragment3"
+ *         android:label="@string/navigation_tab3"
+ *         tools:layout="@layout/fragment_tab3" />
+ *
+ * </navigation>
+ * ```
+ */
+@Navigator.Name("tab_fragment")
+class TabNavigator(
+    private val context: Context,
+    private val manager: FragmentManager,
+    private val containerId: Int
+) : FragmentNavigator(context, manager, containerId) {
+
+    companion object {
+        const val TAG = "TabNavigator"
+
+        const val userShowHideMode = true
+    }
+
+    override fun navigate(
+        destination: Destination,
+        args: Bundle?,
+        navOptions: NavOptions?,
+        navigatorExtras: Navigator.Extras?
+    ): NavDestination? {
+        if (!userShowHideMode) {
+            return super.navigate(destination, args, navOptions, navigatorExtras)
+        } else {
+            if (manager.isStateSaved) {
+                Log.i(TAG, "Ignoring navigate() call: FragmentManager has already saved its state")
+                return null
+            }
+            var className = destination.className
+            if (className[0] == '.') {
+                className = context.packageName + className
+            }
+
+            val ft = manager.beginTransaction()
+
+            var enterAnim = navOptions?.enterAnim ?: -1
+            var exitAnim = navOptions?.exitAnim ?: -1
+            var popEnterAnim = navOptions?.popEnterAnim ?: -1
+            var popExitAnim = navOptions?.popExitAnim ?: -1
+            if (enterAnim != -1 || exitAnim != -1 || popEnterAnim != -1 || popExitAnim != -1) {
+                enterAnim = if (enterAnim != -1) enterAnim else 0
+                exitAnim = if (exitAnim != -1) exitAnim else 0
+                popEnterAnim = if (popEnterAnim != -1) popEnterAnim else 0
+                popExitAnim = if (popExitAnim != -1) popExitAnim else 0
+                ft.setCustomAnimations(enterAnim, exitAnim, popEnterAnim, popExitAnim)
+            }
+
+            val tag = destination.id.toString()
+
+            val currentFragment = manager.primaryNavigationFragment
+            currentFragment?.let {
+                ft.detach(it)
+            }
+
+            manager.findFragmentByTag(tag)?.also {
+                ft.attach(it)
+                ft.setPrimaryNavigationFragment(it)
+            } ?: kotlin.run {
+                val fragment = manager.fragmentFactory.instantiate(context.classLoader, className).apply {
+                    arguments = args
+                }
+                ft.add(containerId, fragment, tag)
+                ft.setPrimaryNavigationFragment(fragment)
+            }
+
+            ft.setReorderingAllowed(true)
+            ft.commitAllowingStateLoss()
+
+            return destination
+        }
+    }
+
+    override fun popBackStack(): Boolean {
+        return if (!userShowHideMode) super.popBackStack() else true
+    }
+
+}
+
+@Suppress("unused")
+class TabNavHostFragment : NavHostFragment() {
+    override fun createFragmentNavigator(): Navigator<out FragmentNavigator.Destination> {
+        return TabNavigator(requireContext(), childFragmentManager, id)
+    }
+}

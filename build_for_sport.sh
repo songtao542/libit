85f1fd1e0043c27d48f63e598811dd152e83e506
songtao.wang <songtao542@gmail.com>
2021-06-13 10:59:34 +0800

    rm build*.sh

--- build_for_sport.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-#./build.sh -b sport -c /Users/developer/src/NbSports-And/NSports/app/libs/
-./build.sh -b sport -d com.sport.day -s library #-c /Users/developer/src/NbSports-And/NSports/app/libs/
--- build_for_vpn.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-./build.sh -b vpn -d com.tsf.client -s library -c /Users/developer/src/AndroidStudioProject/shadowsocks-android/mobile/libs/
